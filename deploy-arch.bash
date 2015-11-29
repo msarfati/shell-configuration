f3df914638ff2674dfa387fa0dada4fc9246c91d
Michael Sarfati <michael.sarfati@utoronto.ca>
2015-11-29 14:56:50 -0500

    arch deploy ok

--- deploy-arch.bash
+++ deploy-arch.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-curl -L "https://raw.githubusercontent.com/msarfati/vimrcs/master/main.vimrc" -o ~/.vimrc
-curl -L "https://raw.githubusercontent.com/msarfati/shell-configuration/master/tmux.conf" -o ~/.tmux.conf
-curl -L "https://github.com/msarfati/shell-configuration/raw/master/zshrc" -o ~/.zshrc
+curl -L "https://github.com/msarfati/shell-configuration/raw/master/vimrc" -o ~/.vimrc
+curl -L "https://github.com/msarfati/shell-configuration/raw/master/tmux.conf" -o ~/.tmux.conf
+curl -L "https://github.com/msarfati/shell-configuration/raw/master/vimrc" -o ~/.zshrc
 
